94c4aadd009a7e49213714956782f76ec10ad109
murmur <43274917+murmurlab@users.noreply.github.com>
2022-12-13 22:21:33 +0000

    a

---
+++ 42cursus/libft/ft_atoi.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+int
+	ft_atoi(const char *str)
+{
+	int	i;
+	int	is_neg;
+	int	res;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
+			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	is_neg = (str[i] == '-') ? -1 : 1;
+	if (is_neg == -1 || str[i] == '+')
+		i++;
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	return (res * is_neg);
+}
