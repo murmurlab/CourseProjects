edb7d756e27a793e5f6e4876f1b02219c6f07494
murmur <43274917+murmurlab@users.noreply.github.com>
2022-12-14 06:42:36 +0000

    a

--- 42cursus/libft/ft_split.c
+++ 42cursus/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 00:44:37 by codespace         #+#    #+#             */
-/*   Updated: 2022/12/14 00:51:59 by codespace        ###   ########.fr       */
+/*   Updated: 2022/12/14 01:38:12 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 char **ft_split(char const *s, char c)
 {
-    // " split  split me    splitt.  "
-    // 'split' '' 'split' 'me' '' '' '' '' 'splitt.' '' ''
+		// " split  split me    splitt.  "
+		// 'split' '' 'split' 'me' '' '' '' 'splitt.' '' ''
+		size_t n;
+		
+		while (*s && *s++ == c)
+			n++;
+		
 }
