c546923fed3b21177882c1e2d52f63b2d3e910a6
murmur <43274917+murmurlab@users.noreply.github.com>
2022-12-14 00:53:13 +0000

    a

--- 42cursus/libft/libft.h
+++ 42cursus/libft/libft.h
@@ -52,5 +52,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, const char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
