cf7ef4512edbf5ecefdb4fb8df623a11476bfd93
murmur <43274917+murmurlab@users.noreply.github.com>
2022-12-14 00:30:46 +0000

    a

--- 42cursus/libft/ft_strtrim.c
+++ 42cursus/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 00:10:20 by codespace         #+#    #+#             */
-/*   Updated: 2022/12/14 00:10:21 by codespace        ###   ########.fr       */
+/*   Updated: 2022/12/14 00:30:41 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (i && ft_strchr(set, s1[i]))
 		i--;
 	return (ft_substr(s1, 0, i + 1));
+}
+
+int	main(void)
+{
+	char	*str = "   xxxxtestttxxx. ";
+	char	*set = "x ";
+
+	printf("%s", ft_strtrim(str, set));
 }
