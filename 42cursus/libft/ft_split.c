a8d4072960944a08dae2aedbf3bf4c84d63aa057
nan0bit <nan0bit@nan0bit.ru>
2022-12-15 03:11:26 +0300

    a

--- 42cursus/libft/ft_split.c
+++ 42cursus/libft/ft_split.c
@@ -6,13 +6,13 @@
 /*   By: nan0bit <nan0bit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 00:44:37 by codespace         #+#    #+#             */
-/*   Updated: 2022/12/14 20:13:26 by nan0bit          ###   ########.fr       */
+/*   Updated: 2022/12/15 01:32:33 by nan0bit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	recurse(char	**pp, size_t i, const char *s, char c, size_t l)
+/* void	recurse(char	**pp, size_t i, const char *s, char c, size_t l)
 {
 	char	*elm;
 
@@ -46,7 +46,146 @@ char	**ft_split(char const *s, char c)
 	pp = malloc(sizeof(char) * (n + 1));
 	recurse(pp + 7, i, s, c, 0);
 	return (pp);
+} */
+
+
+
+
+/* 
+
+
+static int	count_words(const char *str, char c)
+{
+	int i;
+	int trigger;
+
+	i = 0;
+	trigger = 0;
+	while (*str)
+	{
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
+	}
+	return (i);
 }
+
+static char	*word_dup(const char *str, int start, int finish)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((finish - start + 1) * sizeof(char));
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i] = '\0';
+	return (word);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	int		index;
+	char	**split;
+
+	if (!s || !(split = malloc((count_words(s, c) + 1) * sizeof(char *))))
+		return (0);
+	i = 0;
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = word_dup(s, index, i);
+			index = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
+}
+
+
+ */
+
+
+/* 
+
+static size_t	ft_toklen(const char *s, char c)
+{
+	size_t	ret;
+
+	ret = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			++ret;
+			while (*s && *s != c)
+				++s;
+		}
+		else
+			++s;
+	}
+	return (ret);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**ret;
+	size_t	i;
+	size_t	len;
+
+	if (!s)
+		return (0);
+	i = 0;
+	ret = malloc(sizeof(char *) * (ft_toklen(s, c) + 1));
+	if (!ret)
+		return (0);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (*s && *s != c && ++len)
+				++s;
+			ret[i++] = ft_substr(s - len, 0, len);
+		}
+		else
+			++s;
+	}
+	ret[i] = 0;
+	return (ret);
+}
+ */
+
+
+
+char	**ft_split(const char *s, char c)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+
+	while (s[i++])
+	{
+		if (s[i] == c)
+	}
+}
+
+
+
 
 int	main(void)
 {
