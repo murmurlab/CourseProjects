d8bc0decae71ef876a0e70acce031ca6f07aad56
Ahmet Faruk Basaran <ahbasara@k2m06s09.42kocaeli.com.tr>
2022-12-16 02:36:25 +0300

    auto git

--- 42cursus/libft/ft_itoa.c
+++ 42cursus/libft/ft_itoa.c
@@ -6,7 +6,46 @@
 /*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 01:29:57 by ahbasara          #+#    #+#             */
-/*   Updated: 2022/12/16 01:29:58 by ahbasara         ###   ########.fr       */
+/*   Updated: 2022/12/16 02:35:58 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	ft_digloc(int nb, char *src)
+{
+	char	digit;
+
+	digit = 2;
+	while (10 >= (nb / 10))
+	{
+		nb /= 10;
+		digit++;
+	}
+	src = (char *)malloc(sizeof(char) * digit);
+	return (digit);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	char	l;
+
+	l = ft_digloc(n, str);
+	while (--l)
+		str++;
+	*str-- = 0;
+	while (n >= 10)
+	{
+		*str-- = (n % 10) + 48;
+		n /= 10;
+	}
+	*str-- = (n + 48);
+	return (str);
+}
+
+int	main(int argv, char argc[][])
+{
+	(void)argv;
+	printf("res: %d\n", ft_itoa(1234));
+}
