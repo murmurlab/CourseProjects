c3b9ea02dae70baff4959046f11361b71821c110
murmur <43274917+murmurlab@users.noreply.github.com>
2022-12-20 23:31:26 +0000

    a;

--- 42cursus/libft/ft_atoi.c
+++ 42cursus/libft/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 22:21:37 by codespace         #+#    #+#             */
-/*   Updated: 2022/12/17 23:55:40 by ahbasara         ###   ########.fr       */
+/*   Updated: 2022/12/20 21:37:05 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ int	ft_atoi(const char *str)
 	if (*str == '-')
 		is_neg = -1;
 	if ((is_neg == -1 || *str == '+') && str++)
-		;
+	{
+	}
 	res = 0;
 	while (*str >= '0' && *str <= '9')
 		res = (res * 10) + (*str++ - '0');
