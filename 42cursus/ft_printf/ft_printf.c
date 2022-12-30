9dbb3b21620a0d56df7bf6ab72d01ef13fe63147
Ahmet Faruk Basaran <ahbasara@k2m18s09.42kocaeli.com.tr>
2022-12-30 16:44:17 +0300

    a

--- 42cursus/ft_printf/ft_printf.c
+++ 42cursus/ft_printf/ft_printf.c
@@ -6,14 +6,26 @@
 /*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 16:04:43 by ahbasara          #+#    #+#             */
-/*   Updated: 2022/12/30 16:19:35 by ahbasara         ###   ########.fr       */
+/*   Updated: 2022/12/30 16:44:09 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <unistd.h>
 
 int	ft_printf(const char *s, ...)
 {
 	va_list	argl;
-	
+
+	va_start(argl, s);
+	while (*s)
+	{
+		if (*s == '%' && *(s + 1))
+		{
+			if (*++s == 'c')
+				write(1, va_arg(argl, char), 1);
+		}
+		else
+			write(1, s++, 1);
+	}
 }
