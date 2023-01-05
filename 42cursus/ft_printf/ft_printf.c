29c0239fad0309cffd348a3901206564caeefce3
Ahmet Faruk Basaran <ahbasara@k2m13s02.42kocaeli.com.tr>
2023-01-05 08:38:22 +0300

    a

--- 42cursus/ft_printf/ft_printf.c
+++ 42cursus/ft_printf/ft_printf.c
@@ -3,34 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nan0bit <nan0bit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 16:04:43 by ahbasara          #+#    #+#             */
-/*   Updated: 2022/12/31 21:00:11 by nan0bit          ###   ########.fr       */
+/*   Updated: 2023/01/05 08:37:37 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
+#include "utils.h"
+
+size_t	ft_putstr(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		write(1, &s[i++], 1);
+	return (i);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
 int	ft_printf(const char *s, ...)
 {
 	va_list	argl;
+	char	*stock;
 
+	stock = (char *)s;
 	va_start(argl, s);
 	while (*s)
 	{
 		if (*s == '%' && *++s)
 		{
 			if (*s == 'c')
-				write(1, va_arg(argl, char), 1);
+				//ft_putstr(va_arg(argl, char*));
+				write(1, va_arg(argl, char*), 1);
 			else if (*s == 's')
+				//write(1, va_arg(argl, char*), ft_strlen(va_arg(argl, char*)));
+				ft_putstr(va_arg(argl, char*));
+			else if (*s == 'd')
 			{
-				/* code */
+				//ft_putnbr(va_arg(argl, int));
 			}
-			
 		}
 		else
 			write(1, s++, 1);
-	}
+	}	
+	return (s - stock);
 }
