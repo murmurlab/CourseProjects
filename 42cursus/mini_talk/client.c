e1c0684850bf0e08272cf82110ac6e37bada145c
murmurlab <0aeon0@mail.com>
2023-02-18 04:54:41 +0300

    a

--- 42cursus/mini_talk/client.c
+++ 42cursus/mini_talk/client.c
@@ -6,48 +6,59 @@
 /*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:09:19 by ahbasara          #+#    #+#             */
-/*   Updated: 2023/02/17 19:07:04 by ahbasara         ###   ########.fr       */
+/*   Updated: 2023/02/17 20:04:28 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include "../libft/libft.h"
+#include "../ft_printf/spagetti/ft_printf.h"
 
-char	**_message;
-
-void	to_bit(int sig)
+typedef struct s_data
 {
-	(void)sig;
-	static int	i = 0;
+	char *message;
+	int pid;
+} t_data;
 
-	if (i == 8)
+t_data g_data;
+
+void	to_bit(int signal, siginfo_t *info, void	*uc)
+{
+	//usleep(10000);
+	static int	i = 7;
+
+
+	if (i == -1)
 	{
-		if (!*(_message + 2))
+		if (!*g_data.message)
 		{		
 			return ;
 		}
-		(*(2 + _message))++;
-		i = 0;
+		g_data.message++;
+		i = 7;
 	}
-	if ((**(_message + 2) >> i) & 1)
-		kill(*(int *)*(_message + 1), SIGUSR2);
+	if ((*g_data.message >> i) & 1)
+		kill(g_data.pid, SIGUSR2);
 	else
-		kill(*(int *)*(_message + 1), SIGUSR1);
-	i++;
+		kill(g_data.pid, SIGUSR1);
+	i--;
+	//ft_printf("asdasdf\n");
 }
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	int	pid;
+	struct sigaction	act;
 
-	pid = ft_atoi(argv[1]);
-	_message = argv;
-	*(_message + 1) = (char *)&pid;
-	signal(SIGUSR1, to_bit);
-	usleep(200);
-	to_bit(111);
+	g_data.pid = ft_atoi(argv[1]);
+	g_data.message = argv[2];
+	act.sa_sigaction = to_bit;
+	act.sa_flags = SA_SIGINFO;
+	
+	sigaction(SIGUSR1, &act, NULL);
+	//usleep(200);
+	to_bit(123, NULL, NULL);
 	while (1)
 		pause();
 }
--- 42cursus/mini_talk/server.c
+++ 42cursus/mini_talk/server.c
@@ -6,7 +6,7 @@
 /*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:09:17 by ahbasara          #+#    #+#             */
-/*   Updated: 2023/02/17 19:06:54 by ahbasara         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:58:13 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,24 @@
 
 void	handler(int signal, siginfo_t *info, void	*uc)
 {
-	ft_printf("aaaaa %d\n", signal);
+	//usleep(10000);
+	//ft_printf("aaaaa %d\n", signal);
 	(void)uc;
-	static char	c;
-	static char	ct;
+	static unsigned char	c;
+	static unsigned char	ct;
 
+	//ft_printf("%d\n",ct); 
 	c |= (signal == SIGUSR2);
 	if (++ct == 8)
 	{
 		write(1, &c, 1);
 		ct = 0;
+		c = 0;
 	}
 	else
-		c = (c << 1);
-	ft_printf("sending feedback to %d pid\n",info->si_pid);
+		c <<= 1;
+	//ft_printf("sending feedback to %d pid\n",info->si_pid);
+	usleep(200);
 	kill(info->si_pid, SIGUSR1);
 }
 
@@ -40,12 +44,13 @@ int	main(void)
 	
 	pid = getpid();
 	act.sa_sigaction = handler;
+	act.sa_flags = SA_SIGINFO;
+
+	ft_printf("%d\n", pid);
+
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
-	ft_printf("%d\n", pid);
+
 	while (1)
-	{
 		pause();
-	}
-	ft_printf("asda\n");
 }
