87a1395e25023508124e3c1a823199a4b0d91274
murmurlab <0aeon0@mail.com>
2023-02-20 08:37:14 +0300

    a

--- 42cursus/mini_talk/client.c
+++ 42cursus/mini_talk/client.c
@@ -6,9 +6,14 @@
 /*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:09:19 by ahbasara          #+#    #+#             */
-/*   Updated: 2023/02/17 20:04:28 by ahbasara         ###   ########.fr       */
+/*   Updated: 2023/02/20 08:32:44 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* ************************ */
+/*	credit by				*/
+/*		ahbasara & oyumusak	*/
+/* ************************ */
 
 #include <signal.h>
 #include <unistd.h>
@@ -17,47 +22,46 @@
 
 typedef struct s_data
 {
-	char *message;
-	int pid;
-} t_data;
+	char	*message;
+	int		pid;
+}	t_data;
 
-t_data g_data;
+t_data	g_data;
 
 void	to_bit(int signal, siginfo_t *info, void	*uc)
 {
-	//usleep(10000);
-	static int	i = 7;
+	static int	i = 8;
 
-
-	if (i == -1)
+	if (i < 1)
 	{
 		if (!*g_data.message)
-		{		
-			return ;
+		{
+			exit(0);
 		}
 		g_data.message++;
-		i = 7;
+		i = 8;
 	}
-	if ((*g_data.message >> i) & 1)
-		kill(g_data.pid, SIGUSR2);
-	else
-		kill(g_data.pid, SIGUSR1);
 	i--;
-	//ft_printf("asdasdf\n");
+	if ((*g_data.message >> i) & 1)
+	{
+		kill(g_data.pid, SIGUSR2);
+	}
+	else
+	{
+		kill(g_data.pid, SIGUSR1);
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
 	struct sigaction	act;
 
+	(void)argc;
 	g_data.pid = ft_atoi(argv[1]);
 	g_data.message = argv[2];
 	act.sa_sigaction = to_bit;
 	act.sa_flags = SA_SIGINFO;
-	
 	sigaction(SIGUSR1, &act, NULL);
-	//usleep(200);
 	to_bit(123, NULL, NULL);
 	while (1)
 		pause();
--- 42cursus/mini_talk/server.c
+++ 42cursus/mini_talk/server.c
@@ -6,23 +6,25 @@
 /*   By: ahbasara <ahbasara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:09:17 by ahbasara          #+#    #+#             */
-/*   Updated: 2023/02/17 19:58:13 by ahbasara         ###   ########.fr       */
+/*   Updated: 2023/02/20 08:35:13 by ahbasara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "../ft_printf/spagetti/ft_printf.h"
+
+/* #include <fcntl.h>
+int fd; */
 
 void	handler(int signal, siginfo_t *info, void	*uc)
 {
-	//usleep(10000);
-	//ft_printf("aaaaa %d\n", signal);
-	(void)uc;
 	static unsigned char	c;
 	static unsigned char	ct;
+	static int				temp = 0;
 
-	//ft_printf("%d\n",ct); 
+	(void)uc;
 	c |= (signal == SIGUSR2);
 	if (++ct == 8)
 	{
@@ -32,25 +34,26 @@ void	handler(int signal, siginfo_t *info, void	*uc)
 	}
 	else
 		c <<= 1;
-	//ft_printf("sending feedback to %d pid\n",info->si_pid);
-	usleep(200);
-	kill(info->si_pid, SIGUSR1);
+	if (temp == 0)
+		temp = info->si_pid;
+	if (temp != info->si_pid && info->si_pid != 0)
+			temp = info->si_pid;
+	kill(temp, SIGUSR1);
 }
 
 int	main(void)
 {
 	struct sigaction	act;
 	int					pid;
-	
+
 	pid = getpid();
 	act.sa_sigaction = handler;
 	act.sa_flags = SA_SIGINFO;
-
 	ft_printf("%d\n", pid);
-
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
-
 	while (1)
+	{
 		pause();
+	}
 }
