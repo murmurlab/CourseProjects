0bd3d5a7da3a666e647779e25f4fd35bc5cfcfa9
Your Name <you@example.com>
2023-06-08 22:38:02 +0300

    o

--- main/so_long2/game_utils.c
+++ main/so_long2/game_utils.c
@@ -1,8 +1,35 @@
 #include "game.h"
 
+int	check_map(char *line, size_t *x_len, size_t *y_len);
+
 void	load_map(t_game *s_game, char **c)
 {
-	p("%s", *(c + 1));
-	p("%s", *(c + 1));
-	// multiRowRead();
+	int		fd_map;
+	char	*line;
+	size_t	x_len;
+	size_t	y_len;
+
+	fd_map = open(*(c + 1), O_RDONLY);
+	line = multiRowRead(fd_map);
+	x_len = xstrlen(line);
+	y_len = 1;
+	while (*line)
+	{
+		if (*(line++) != 1)
+			return (1);
+	}
+	while (line)
+	{
+		check_map(line, &x_len, &y_len);
+		x_len = xstrlen(line);
+		line = multiRowRead(fd_map);
+	}
+}
+
+int	check_map(char *line, size_t *x_len, size_t *y_len)
+{
+	if (*(line + 0) != '1' || *(line + (*x_len - 1)) != '1')
+		return (1);
+	if (*x_len != xstrlen(line))
+		return (1);
 }
