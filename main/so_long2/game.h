ff3f569bf0a6ffb134ba414f14daed52975682f2
Your Name <you@example.com>
2023-06-12 13:12:45 +0300

    malloc map

--- main/so_long2/game.h
+++ main/so_long2/game.h
@@ -8,15 +8,23 @@ typedef struct s_obj
 	int	x;
 	int	y;
 }		t_obj;
-
 typedef struct s_game
 {
 	char	**map;
 	char	**p;
 	t_obj	*e;
 }		t_game;
+struct s_read_map
+{
+	char	*line;
+	size_t	old_x_len;
+	size_t	y_len;
+	int		i;
+	t_game	*s_game;
+};
 
-int	load_map(t_game *s_game, char **c);
+
+int	load_map(struct s_read_map *s_read_map, char **c);
 int	init(t_game *s_game, char **c);
 
 #endif
--- main/so_long2/game_utils.c
+++ main/so_long2/game_utils.c
@@ -1,12 +1,36 @@
 #include "game.h"
 
-int	check_map(char *line, size_t *x_len, size_t *y_len, t_game *s_game);
+int	check_lines\
+(
+	struct s_read_map *s_read_map
+)
+{
+	if (*(s_read_map->line + 0) != '1' || *(s_read_map->line + \
+	(s_read_map->old_x_len - 2)) != '1')
+		return (1);
+	if (s_read_map->old_x_len != xstrlen(s_read_map->line))
+	{
+		if ((s_read_map->y_len == s_read_map->i) \
+		&& (xstrlen(s_read_map->line) == (s_read_map->old_x_len - 1)))
+			;
+		else
+		{
+			return (1);
+		}
+	}
+	s_read_map->s_game->map[s_read_map->y_len - 1] = \
+	malloc(s_read_map->old_x_len);
+	xstrlcpy(s_read_map->s_game->map[s_read_map->y_len - 1], \
+	s_read_map->line, s_read_map->old_x_len);
+	return (0);
+}
 
 int	get_y_len(char *fname)
 {
 	int	fd_map;
 	int	y;
 
+	y = 0;
 	fd_map = open(fname, O_RDONLY);
 	while (multiRowRead(fd_map))
 		y++;
@@ -14,40 +38,44 @@ int	get_y_len(char *fname)
 	return (y);
 }
 
-int	load_map(t_game *s_game, char **c)
+int	check_y_border(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] != '\n')
+	{
+		if (line[i++] != '1')
+			return (1);
+	}
+	return (0);
+}
+
+int	load_map(struct s_read_map *s_read_map, char **c)
 {
 	int		fd_map;
-	char	*line;
-	size_t	x_len;
-	size_t	y_len;
 
 	fd_map = open(*(c + 1), O_RDONLY);
-	line = multiRowRead(fd_map);
-	y_len = get_y_len(*(c + 1));
-	x_len = xstrlen(line);
-	while (line[y_len] != '\n')
+	(*s_read_map).line = multiRowRead(fd_map);
+	(*s_read_map).old_x_len = xstrlen((*s_read_map).line);
+	(*s_read_map).y_len = get_y_len(*(c + 1));
+	check_y_border((*s_read_map).line);
+	(*s_read_map).i = 1;
+	(*s_read_map).s_game->map = \
+	(char *)malloc((*s_read_map).y_len * sizeof(char *));
+	p("%p", (*s_read_map).s_game->map[0]);
+	while (1)
 	{
-		if (line[y_len++] != '1')
+		if (check_lines(s_read_map))
 			return (1);
+		(*s_read_map).old_x_len = xstrlen((*s_read_map).line);
+		if ((*s_read_map).y_len > (*s_read_map).i)
+			(*s_read_map).line = multiRowRead(fd_map);
+		else
+			break ;
+		(*s_read_map).i++;
 	}
-	while (line)
-	{
-		if (check_map(line, &x_len, &y_len, s_game))
-			return (1);
-		x_len = xstrlen(line);
-		line = multiRowRead(fd_map);
-	}
+	check_y_border((*s_read_map).line);
 	close(fd_map);
 	return (0);
 }
-
-int	check_map(char *line, size_t *x_len, size_t *y_len, t_game *s_game)
-{
-	if (*(line + 0) != '1' || *(line + (*x_len - 2)) != '1')
-		return (1);
-	if (*x_len != xstrlen(line))
-		return (1);
-	xstrlcpy(s_game->map[*y_len - 1], line, *x_len);
-	return (0);
-}
-
