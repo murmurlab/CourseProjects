d055fceaa5c8937bac92fb7e14551fa316663991
marvin <marvin@42.fr>
2024-03-21 23:08:28 +0300

    t

--- main/cpp_practice/fourth_practice/ex01/main.cpp
+++ main/cpp_practice/fourth_practice/ex01/main.cpp
@@ -1,40 +1,40 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(int argc, char const *argv[]){
-ClapTrap    one("one");
-ClapTrap    two("two");
-ClapTrap    three("three");
+ScavTrap    one("one");
+// ScavTrap    two("two");
+// ScavTrap    three("three");
 
-one.attack("two");
-cout << endl;
-cout << endl;
-two.takeDamage(6);
-two.beRepaired(6);
-two.beRepaired(6);
-two.takeDamage(6);
-two.takeDamage(6);
-two.takeDamage(6);
-two.takeDamage(6);
-cout << endl;
-cout << endl;
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.beRepaired(0);
-one.attack("noneeeeee");
-cout << endl;
-cout << endl;
-one.takeDamage(155);
-cout << endl;
-cout << endl;
-three = one; // both name one
-three.attack("when dead"); //name one
-one.attack("when dead"); //name one
+// one.attack("two");
+// cout << endl;
+// cout << endl;
+// two.takeDamage(6);
+// two.beRepaired(6);
+// two.beRepaired(6);
+// two.takeDamage(6);
+// two.takeDamage(6);
+// two.takeDamage(6);
+// two.takeDamage(6);
+// cout << endl;
+// cout << endl;
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.beRepaired(0);
+// one.attack("noneeeeee");
+// cout << endl;
+// cout << endl;
+// one.takeDamage(155);
+// cout << endl;
+// cout << endl;
+// three = one; // both name one
+// three.attack("when dead"); //name one
+// one.attack("when dead"); //name one
 return 0;
 }
