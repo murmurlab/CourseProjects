9ed0760fac5bd17513ceae2442e70039a1ec6cc7
ahbasara <ahbasara@student.42istanbul.com.tr>
2024-05-10 00:29:47 +0300

    Add settings.json and implement AMateria class

---
+++ main/cpp_practice/fifth_practice/ex03/AMateria.cpp
@@ -0,0 +1,12 @@
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+
+void AMateria::use(ICharacter &target)
+{
+    // cout << "* shoots an ice bolt at " << target.getName() << " *" << endl;
+}
+
+std::string const &AMateria::getType() const{
+    // return this->_type;
+}
---
+++ main/cpp_practice/fifth_practice/ex03/AMateria.hpp
@@ -0,0 +1,35 @@
+#ifndef AMATERIA_H
+# define AMATERIA_H
+# if defined(__linux__)
+/* Linux. --------------------------------------------------- */
+
+# elif defined(__APPLE__)
+/* OSX. --------------------------------------------------- */
+
+# endif
+
+# define RED				"\x1B[31m"
+# define GREEN				"\x1B[32m"
+# define YELLOW				"\x1B[33m"
+# define BLUE				"\x1B[34m"
+# define RESET				"\x1B[0m"
+
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+class AMateria
+{
+protected:
+
+public:
+    AMateria(std::string const &type);
+    std::string const &getType() const;
+    virtual AMateria *clone() const = 0;
+    virtual void use(ICharacter &target);
+};
+
+#endif
