0d2303c3fa66877b96c57413d93e4b758dd13500
ahbasara <ahbasara@student.42istanbul.com.tr>
2024-09-25 20:01:43 +0300

    Fix pointer casting in Serializer class

--- main/cpp_practice/cpp6/ex01/Serializer.cpp
+++ main/cpp_practice/cpp6/ex01/Serializer.cpp
@@ -4,9 +4,9 @@ Serializer::Serializer(Serializer& cpy) {(void)cpy;}
 Serializer::Serializer() {}
 
 uintptr_t	Serializer::serialize(Data* ptr) {
-	return uintptr_t(ptr);
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data*	Serializer::deserialize(uintptr_t raw) {
-	return (Data *)(raw);
+	return reinterpret_cast<Data *>(raw);
 }
