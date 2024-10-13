0792987f4f99bc355e6d183be89fd35c81a1dcb9
murmur <0aeon0@mail.com>
2024-10-14 02:08:32 +0300

    todo: validate field

---
+++ main/cpp_practice/cpp9_try1/ex00/BitcoinExchange.cpp
@@ -0,0 +1,157 @@
+#include "BitcoinExchange.hpp"
+
+static string	trim(string const& str) {
+	return str.substr(str.find_first_not_of(WS), str.find_last_not_of(WS)+1);	
+}
+
+std::vector<string> BitcoinExchange::split(string& line, char sep) {
+	std::vector<string> ret;
+	
+	std::istringstream iss(line.c_str());
+	size_t l = line.length();
+	char* node = new char[l];
+	string trimmed;
+	for (bool stat; ((stat = iss.getline(node, l, sep)) || !(trimmed = trim(iss.str())).empty());) {
+		// cout << "stat:" << stat << " eof:"<< iss.eof() << " s: \"" << line << "\" - " << node << endl<< endl;
+		if (iss.eof() && !stat) break;
+		if (!stat) {
+			ret.push_back(trimmed);
+			ret.push_back(string(""));
+			break ;
+		} else {
+			// cout << "split: " << node << endl;
+			string s(node);
+			ret.push_back(trim(s));
+		}
+	}
+	delete[] node;
+	return (ret);
+}
+
+BitcoinExchange::BitcoinExchange(string const& csv_file) {
+	db = csv_to_vec(csv_file, ',');
+	// if ("date,exchange_rate" != line)
+	// 	throw (runtime_error(S_E_CORRUPTED_DB));
+	
+	// std::istringstream iss(line.c_str());
+	// size_t l = line.length(); 
+	// char* node = new char[l];
+	// for (; iss.getline(node, l, seperator);) {
+	// 	string untrimmed(node);
+	// 	string trimmed;
+	// 	std::istringstream str(untrimmed);
+	// 	str >> trimmed;
+	// 	// cout << trimmed << endl;
+	// 	t.column_types->push_back(
+	// 		(trimmed == string("date")) ?			date :
+	// 		(trimmed == string("value")) ?			value :
+	// 		(trimmed == string("exchange_rate")) ?	exchange_rate :
+	// 											unknown_type
+	// 	);
+	// 	cout << "\t" << trimmed << endl;
+	// 	cout << "\t\t typedef " << trimmed << " " << t.column_types->back() << endl;
+	// }
+	// t.width = t.column_types->size();
+	// t.c = new std::vector<void*>(t.width * t.height);
+	// // table[(399 * coloumns) + 1] = NULL;
+
+	// for (size_t row(0); std::getline(readable, line); row++) {
+	// 	cout << line << endl;
+	// 	std::istringstream iss_tmp(line.c_str());
+	// 	int col = t.column_types->front();
+	// 	size_t le = line.length();
+	// 	char* node = new char[le];
+		
+	// 	bool cont = true;
+	// 	for (; cont; col++) {
+	// 		cont = iss_tmp.getline(node, le, seperator);
+
+	// 		if (!cont)
+	// 			t.c->at((row * t.width) + col) = NULL;
+	// 		else {
+	// 			// (coloumn_types[col] == date) ?			("NULL") :
+	// 			// (coloumn_types[col] == exchange_rate) ?	(NULL) :
+	// 			// 											(NULL)
+	// 			string untrimmed(node);
+	// 			string trimmed;
+	// 			std::istringstream str(untrimmed);
+	// 			str >> trimmed;
+	// 			cout << "\t" << "<" << t.column_types->at(col) << ">" << trimmed << endl;
+	// 			t.c->at((row * t.width) + col) = serialize(t.column_types->at(col) ,(char *)trimmed.c_str());
+	// 		}
+	// 		// if 
+	// 		// table[(row * coloumns) + col] = NULL;
+	// 	}
+	// }
+	// return t;
+}
+
+std::vector<string> BitcoinExchange::csv_to_vec(string const& csv_file, char del) {
+	std::vector<string> ret;
+	
+	std::ifstream db_file(csv_file.c_str());
+	if (!db_file) throw(runtime_error(S_E_CANNOT_OPEN_FILE));
+	std::ostringstream content;
+	content << db_file.rdbuf();
+	string all(content.str());
+	std::istringstream all_lines(all);
+
+	string	head;
+
+	std::getline(all_lines, head);
+	std::vector<string> list = split(head, del);
+	if (list.size() != 2) {
+		throw (runtime_error(S_E_CORRUPTED_DB": 2 head err"));
+	}
+	if (del == '|') {
+		if (list[0] != "date" || list[1] != "value")
+			throw (runtime_error(S_E_CORRUPTED_DB": '|' head err"));			
+	} else if (del == ',') {
+		if (list[0] != "date" || list[1] != "exchange_rate")
+			throw (runtime_error(S_E_CORRUPTED_DB": ',' head err"));
+	}
+
+	// cout << list[0] << " / " << list[1] << endl;
+
+	for (string line; std::getline(all_lines, line);) {
+		list = split(line, del);
+		if (list.size() < 2) {
+			ret.push_back(list[0]);
+			ret.push_back(string(""));
+			// throw (runtime_error(S_E_CORRUPTED_DB));
+		} else {
+			ret.insert(ret.end(), list.begin(), list.end());
+		}
+		// cout << list[0] << " / " << list[1] << endl;
+		// cout << line << endl;
+		// for (; line.getline();)
+	}
+	return ret;
+}
+
+
+
+void BitcoinExchange::calculate(string inp_file) {
+	// list(db);
+	std::vector<string> db_in = csv_to_vec(inp_file, '|');
+	for (std::vector<string>::iterator it = db_in.begin(); it != db_in.end(); it+=2) {
+		for (std::vector<string>::iterator it2 = db.begin(); it2 != db.end(); it2+=2) {
+			if (*it == *it2)
+				cout << *it2 << " " << *(it2 + 1) << endl;
+		}
+		// cout << *it << " " << *(it + 1) << endl;
+	}
+
+	// list(db_in);
+}
+
+static void p(string& s) {
+	cout << s << endl;
+}
+
+void BitcoinExchange::list(std::vector<string>& vec) {
+	for (std::vector<string>::iterator it = vec.begin(); it != vec.end(); it+=2) {
+		cout << *it << " " << *(it + 1) << endl;
+	}
+	// std::for_each(db.begin(), db.end(), p);
+}
---
+++ main/cpp_practice/cpp9_try1/ex00/BitcoinExchange.hpp
@@ -0,0 +1,74 @@
+#if !defined(BITCOIN_EXCHANGE_HPP)
+# define BITCOIN_EXCHANGE_HPP
+
+# include "common.hpp"
+
+# include <ctime>
+# include <fstream>
+# include <typeinfo>
+# include <vector>
+# include <algorithm>
+# include <sstream>
+# include <stdexcept>
+# include <string>
+# include <iostream>
+# include <iterator>
+# include <ostream>
+# include <iomanip>
+
+// using std::map;
+
+#define DISCRIMINATOR_1				','
+#define DISCRIMINATOR_2				'|'
+
+#define DEFAULT_CSV_FILE			"./data.csv"
+
+#define S_E_CORRUPTED_DB			"corrupted db"
+#define S_E_SUCCES					"error of succes"
+#define S_E_INSUFFICIENT_ARGUMENTS	"least 2 arg"
+#define S_E_CANNOT_OPEN_FILE		"cannot open file"
+
+
+typedef enum e_column {
+	date = 0, // string
+	value, // float
+	exchange_rate, // float
+	unknown_type
+}		t_column_idx;
+
+typedef string				t_date;
+typedef float				t_exchange_rate;
+typedef float				t_value;
+
+// # define (type, id) (type*)id
+// # define EXP(type, column, ) type* 
+// 1typedef string
+
+// typedef float t_price;
+
+// typedef void**	t_row;
+// typedef void*	t_column;
+
+// typedef struct	s_csv {
+// 	std::vector<void*>*			c; // possible w/o vector, with new void*[n]
+// 	std::vector<t_column_idx>*	column_types;
+// 	size_t						width;
+// 	size_t						height;
+// }				t_csv;
+
+
+
+class BitcoinExchange {
+	public:
+		BitcoinExchange(string const& csv_file = string(DEFAULT_CSV_FILE));
+
+		typedef float t_price;
+		void calculate(string inp_file);
+		std::vector<string>	csv_to_vec(string const& csv_file, char del);
+		void list(std::vector<string>& vec);
+		std::vector<string>		db;
+	private:
+		std::vector<string> split(string& line, char sep);
+};
+
+#endif // BITCOIN_EXCHANGE_HPP
